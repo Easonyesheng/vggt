46dc46a7e9c0de44618c5b70b37d641e8f593607
Easonyesheng <preacher@sjtu.edu.cn>
2026-03-07 13:26:14 +0800

    update

--- __init__.py
+++ __init__.py
@@ -2,8 +2,9 @@
 Author: Easonyesheng preacher@sjtu.edu.cn
 Date: 2025-11-18 12:13:49
 LastEditors: Easonyesheng preacher@sjtu.edu.cn
-LastEditTime: 2025-11-21 16:57:36
+LastEditTime: 2026-01-09 11:58:43
 FilePath: /nerfstudio/research/ref_ray/third_party/vggt/__init__.py
 Description: 
 '''
 from .vggt.models.vggt import VGGT
+from .vggt.utils.pose_enc import pose_encoding_to_extri_intri
